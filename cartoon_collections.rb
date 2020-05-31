38175f39634470567a4cc9d75b2e7ffd2cbc57d8
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-31 15:43:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-	if array.include?("cheddar") || array.include?("gouda") || array.include?("camembert")
+	if array.include?(["cheddar","gouda","camembert"])
 		array.find do |cheese|
         cheese="cheddar" || cheese="gouda" || cheese="camembert"
     end
