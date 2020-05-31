60855f6b164d71c77291b540793f0c9180c8dd54
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-31 15:49:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(array)
 	if array.include?(["cheddar","gouda","camembert"])
-		array.find {|cheese| cheese == "cheedar" || cheese == "gouda" || cheese == "camembert"}
+		array.find {|cheese| cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"}
   else
     nil
   end
